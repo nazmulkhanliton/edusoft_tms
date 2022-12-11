bd6143d95794fb4dd2f7a83c07db07722047e795
nazmulkhanliton <liton079@gmail.com>
2022-12-11 16:27:38 +0600

    1st

---
+++ android/app/src/main/kotlin/com/example/edusoft_tms/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.edusoft_tms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/onBoardingScreen/onBoarding_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+        'Khan',
+        style: TextStyle(
+          color: Colors.lightBlue,
+        ),
+      ),
+    );
+  }
+}
